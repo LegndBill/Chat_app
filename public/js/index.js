9eaf8cf0aeb0342e523ac9f908d80661fdf8b68a
Nicholas <bobtheturtle271@gmail.com>
2018-02-15 20:46:02 -0500

    basic message setup

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('connected to server');
-  socket.emit('createMessage',{
-    from: 'Joe',
-    text: 'I hate you'
-  });
+
 });
 socket.on('disconnect', () => {
   console.log('disconnected from server');
