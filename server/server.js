9e86c00cb8d22f880cb366a489286069e2bead08
Nicholas <bobtheturtle271@gmail.com>
2018-02-18 20:31:37 -0500

    very bascic UI

--- server/server.js
+++ server/server.js
@@ -19,13 +19,15 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) =>{
+  socket.on('createMessage', (message, callback) =>{
     console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text)
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('hello mate');
+
       // from: message.from,
       // text: message.text,
       // createdAt: new Date().getTime()
-    );
+
 
   });
 
